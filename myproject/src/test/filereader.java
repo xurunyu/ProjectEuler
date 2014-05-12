6079e5276da69f1487dcc168fb299f99546ece7a
xurunyu <xurunyu@hotmail.com>
2014-05-12 22:33:15 +0800

    filereader

--- myproject/src/test/filereader.java
+++ myproject/src/test/filereader.java
@@ -1,5 +1,18 @@
 package test;
 
-public class filereader {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class filereader {
+	public static void main(String[] args) throws IOException{
+		FileInputStream it = new FileInputStream(new File("src/test/test.txt"));
+		InputStreamReader reader = new InputStreamReader(it);
+		int ch = 0;
+		while((ch = reader.read()) != -1 ){
+			System.out.println((char)ch);
+		}
+	}
 }
